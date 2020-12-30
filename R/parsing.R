3c310e5116af4d0a0e2148f8e32beba09d5fdd3d
ben weiher <bweiher@pm.me>
2020-12-30 15:51:52 -0800

    https://github.com/bweiher/pelotonR/pull/10

--- R/parsing.R
+++ R/parsing.R
@@ -52,7 +52,7 @@ parse_dates <- function(dataframe, tz = base::Sys.timezone()) {
   for (i in seq_along(names)) {
     name <- names[i]
     # TODO parse inner list too
-    true[[i]] <- grepl(pattern = "^1[0-9]{9}", x = dataframe[[name]]) && !is.list(dataframe[[name]]) && !name %in% c("id", "facebook_id")
+    true[[i]] <- grepl(pattern = "^1[0-9]{9}", x = dataframe[[name]]) && !is.list(dataframe[[name]]) && !name %in% c("peloton_id", "id", "facebook_id")
   }
   vars <- names[true]
   dplyr::mutate_at(dataframe, vars, fn)
